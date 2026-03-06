ae6ba4ddf5e3d07b09c89ad5de308e7777da4aa9
chitraksh jain <160010835+chitraksh-jain@users.noreply.github.com>
2026-03-06 16:29:49 +0530

    add axios timeout for render wakeup

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,11 @@ import Editor from '@monaco-editor/react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import axios from 'axios';
+
 import 'reactflow/dist/style.css';
 import './App.css';
 
-
+axios.defaults.timeout = 60000;
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
